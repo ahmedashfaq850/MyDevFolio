63d24c182ad6fd39393e0975d0ad0e80365da19a
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2023-03-12 14:30:41 +0500

    updated

---
+++ data/ConstantData.js
@@ -0,0 +1,100 @@
+export const skills = [
+  {
+    name: 'TypeScript',
+    image: '/typescript.svg',
+  },
+  {
+    name: 'JavaScript',
+    image: '/JavaScript.svg',
+  },
+  {
+    name: 'Node JS',
+    image: '/nodejs.svg',
+  },
+  {
+    name: 'Tailwind CSS',
+    image: '/tailwind.svg',
+  },
+  {
+    name: 'Next JS',
+    image: '/nextjs.png',
+  },
+  {
+    name: 'Figma',
+    image: '/figma.svg',
+  },
+  {
+    name: 'Material UI',
+    image: '/material.svg',
+  },
+  {
+    name: 'Redux Toolkit',
+    image: '/redux.svg',
+  },
+  {
+    name: 'React JS',
+    image: '/react.png',
+  },
+  {
+    name: 'Git & GitHub',
+    image: '/git.svg',
+  },
+  {
+    name: 'Mongo DB',
+    image: '/mongo.svg',
+  },
+  {
+    name: 'Firebase',
+    image: '/firebase.svg',
+  },
+]
+
+export const experience = [
+  {
+    title: 'Front-End Engineer Intern',
+    company: 'Radical X | United States',
+    date: 'July 2021 - Dec 2021',
+    stack: ['React Js', 'Material Ui', 'Firebase', 'Responsive Layouts'],
+  },
+  {
+    title: 'Front-End Engineer',
+    company: 'ZHAD Developers | Karachi, Pakistan',
+    date: 'Jan 2021 - Aug 2021',
+    stack: ['React Js', 'Tailwind Css', 'Redux', 'API’s Integration'],
+  },
+]
+
+export const projects = [
+  {
+    title: 'Uber Eats Clone',
+    description:
+      'A Uber Eats Clone  built using React JS and styled with Tailwind CSS. The application is designed to  be completely responsive, ensuring a seamless user experience across all devices.',
+    image: '/UberEatClone.svg',
+    Githublink: 'https://github.com/ahmedashfaq850/ubereats',
+    Websitelink:
+      'https://6384ec076260b02b86370952--magical-kitten-3599ea.netlify.app/',
+    tags: ['React JS', 'Tailwind CSS'],
+  },
+
+  {
+    title: 'Booking App UI',
+    description:
+      'A Booking App UI built using React JS and styled with Tailwind CSS. The application is designed to  be completely responsive, ensuring a seamless user experience across all devices.',
+    image: '/BookingApp.svg',
+    Githublink: 'https://github.com/ahmedashfaq850/BookingApp-ui-in-REACT-JS',
+    Websitelink:
+      'https://booking-app-ui-in-react-3h5vyrkak-ahmedashfaq850.vercel.app/',
+    tags: ['React JS', 'Tailwind CSS'],
+  },
+
+  {
+    title: 'Crypto Currency Tracker',
+    description:
+      'A Crypto Currency Tracker built using React JS and styled with Tailwind CSS. The application is designed to  be completely responsive, ensuring a seamless user experience across all devices.',
+    image: '/CryptoIcon.svg',
+    Githublink: 'https://github.com/ahmedashfaq850/CryptoDashboard',
+    Websitelink:
+      'https://637a37fa850b2c439f0cc775--fancy-froyo-09135a.netlify.app/',
+    tags: ['React JS', 'Tailwind CSS'],
+  },
+]
