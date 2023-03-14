4a52d6b9280b2a9887f5291f58a24018e49f2782
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2023-03-14 20:12:57 +0500

    integrate vercel live analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react';
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <><Header /> <Component{...pageProps} /> <Footer/> </>
+  return <><Header /> <Component{...pageProps} /> <Analytics /> <Footer /> </>
 
 }
