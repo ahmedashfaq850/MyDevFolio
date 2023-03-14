6dbb6f06d307221f604eb2763603a469706df430
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2023-03-14 18:49:23 +0500

    update some minor changes

--- components/ExperienceSection.tsx
+++ components/ExperienceSection.tsx
@@ -8,11 +8,11 @@ const ExperienceSection = () => {
       <h1 className='text-center text-white text-[30px] md:text-[48px] font-bold flex items-center gap-2 justify-center'>My Experience<Image className='inline-flex' src="/experienceLogo.svg" width={50} height={50} alt="" /></h1>
       <div className='flex justify-center mr-[185px] md:mr-[300px] mt-[5px]'><Image src="/newstripe.png" width="63" height="13" alt="" /></div>
       <div className='flex justify-center items-center mt-[80px] text-white'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-12 '>
           {experience.map((item, idx) => (
-            <div className='experience-container' key={idx}>
+            <div className='experience-container bg-red-300' key={idx}>
               <h1 className='text-[25px] font-[600] mb-[5px]'>{item.title}</h1>
-              <h2 className='text-[20px] font-[500] mb-[5px]'>{item.company}</h2>
+              <h2 className='text-[18px] font-[500] mb-[5px]'>{item.company}</h2>
               <p className='text-[15px] font-[300] mb-[30px]'>{item.date}</p>
               <p className='text-[15px] font-bold mb-[5px]'>Tech Stack</p>
               <div className='flex flex-wrap gap-3 text-[14px]'>
--- components/TestimonialSection.tsx
+++ components/TestimonialSection.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 const TestimonialSection = () => {
   return (
     <div className='mt-[50px] mb-[50px] relative'>
-      <h1 className='text-center text-white text-[30px] md:text-[48px] font-bold flex items-center gap-2 justify-center'>What Client Say about Me<Image className='inline-flex' src="/ClientIcon.svg" width={50} height={50} alt="" /></h1>
-      <div className='flex justify-center mr-[325px] md:mr-[525px] mt-[5px]'><Image src="/newstripe.png" width="63" height="13" alt="" /></div>
+      <h1 className='text-center text-white text-[20px] md:text-[48px] font-bold flex items-center gap-2 justify-center'>What Client Say about Me<Image className='inline-flex' src="/ClientIcon.svg" width={50} height={50} alt="" /></h1>
+      <div className='flex justify-center mr-[220px] md:mr-[525px] mt-[5px]'><Image src="/newstripe.png" width="63" height="13" alt="" /></div>
       <div className='flex flex-col justify-center items-center gap-[25px]'>
         <p className=' m-auto text-[10px] md:text-[18px] text-center w-[90%] md:w-[65%] mt-[30px] text-white leading-8'>I had a fantastic experience working with Ahmed on my front-end project. From the very beginning,
           they were responsive, communicative, and professional. They took the time to  listen  to my needs
