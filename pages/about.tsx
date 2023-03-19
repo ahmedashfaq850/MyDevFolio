4a4f09ff50d14e09b927d243c2efa5848994f743
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2023-03-19 23:01:48 +0500

    updated

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,7 +10,7 @@ const About = () => {
                     <h1 className='text-[25px] md:text-[40px] font-black mb-[5px]'>Ahmed Ashfaq</h1>
                     <h4 className='text-[15px] md:text-[20px] underline font-[500]'>Full Stack - Software Engineer</h4>
                 </div>
-                <p className='text-center leading-7 text-[10px] md:text-[15px] mt-[10px]'>
+                <p className='text-center leading-5 text-[10px] md:text-[15px] mt-[10px]'>
                     <p>Hello! I&apos;m Ahmed Ashfaq, a Full Stack Software Engineer with extensive experience in Node Js, Next Js, React JavaScript, Typescript, and MnogoDB. I&apos;m also currently learning about the exciting world of web 3.0 and blockchain technology, and have a keen interest in developing decentralized web applications.</p>
 
                 </p>
